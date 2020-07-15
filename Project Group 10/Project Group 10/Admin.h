26cfb1acec1a1931ef84378fad1a2f73cf6f5ce0
thinhphuoc <63508962+thinhphuoc@users.noreply.github.com>
2020-07-15 22:08:36 +0700

    Template Project

---
+++ Project Group 10/Project Group 10/Admin.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Admin
+{
+private:
+	string name;
+	string usename;
+	string password;
+
+
+
+};
+
---
+++ Project Group 10/Project Group 10/Project Group 10.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "Admin.h"
+#include "User.h"
+using namespace std;
+
+int main()
+{
+    
+
+
+}
+
